7549be878e88d237ac1218c451cb83a2f0c175a3
rex-vero <142157808+rex-vero@users.noreply.github.com>
2025-03-15 17:57:28 +0330

    edit logic will be added

--- src/components/main/SingleCard.js
+++ src/components/main/SingleCard.js
@@ -6,20 +6,22 @@ import axios from 'axios';
 
 const SingleCard = ({ item, now }) => {
     const { card, setCard, setFilterData } = useContext(DataContext);
-    const handleDelete = () => {
-        const deleteData = async () => {
-            try {
-                const res = await axios.delete(`http://localhost:8000/profiles/${item.id}`);
-                if (res.status === 200) {
-                    const newList = card.filter(prof => prof.id !== item.id);
-                    setCard(newList);
-                    setFilterData(newList);
-                }
-            } catch ({ message }) {
-                console.log(message);
+    const handleDelete = async () => {
+        try {
+            const res = await axios.delete(`http://localhost:8000/profiles/${item.id}`);
+            if (res.status === 200) {
+                const newList = card.filter(prof => prof.id !== item.id);
+                setCard(newList);
+                setFilterData(newList);
             }
+        } catch ({ message }) {
+            console.log(message);
         }
-        deleteData();
+    }
+    const handleEdit = async () => {
+        const temp = [...card]
+        const index = temp.findIndex(prof => prof.id === item.id);
+        console.log(temp[index]);
     }
     return (
         <div className={`card ${styles.bg}`}>
@@ -28,7 +30,7 @@ const SingleCard = ({ item, now }) => {
                 <h5 className="card-title text-center">{item.title}</h5>
                 <p className="card-text text-center">{item.text}</p>
                 <div className='d-flex justify-content-around'>
-                    <button className='w-25 border-1 btn-outline-success btn rounded-5 px-2 bi bi-pen' />
+                    <button onClick={handleEdit} className='w-25 border-1 btn-outline-success btn rounded-5 px-2 bi bi-pen' />
                     <Link to={now === 'home' ? `/profiles/${item.id}` : `/`} className={`w-25 border-1 btn-outline-info btn rounded-5 px-2 bi ${now === 'home' ? `bi-eye` : `bi-arrow-90deg-left`}`} />
                     <button onClick={handleDelete} className='w-25 border-1 btn-outline-danger btn rounded-5 px-2 bi bi-trash' />
                 </div>
@@ -36,5 +38,5 @@ const SingleCard = ({ item, now }) => {
         </div>
     );
 }
-
+//میتونم با یه استیت اینپوت هارو صدا کنم و مقدار بگیرم و بعد ادیتو هندل کنم
 export default SingleCard;

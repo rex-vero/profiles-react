0a93b73978773cca9b3bd42b8e0f12d05112332e
unknown <1rexvero1@gmail.com>
2025-03-19 10:37:47 -0700

    delete modal fixed

--- src/components/modal/Delete.js
+++ src/components/modal/Delete.js
@@ -20,7 +20,7 @@ const Delete = ({ item }) => {
     }
     return (
         <div className="d-flex flex-column align-items-center px-2">
-            <span className="my-2">Do You Want To Delete?!</span>
+            <span className="my-2">Do You Want To Delete {item.title}?!</span>
             <div className="d-flex my-2 gap-4">
                 <button onClick={handleDelete} className="btn btn-outline-success">Yes</button>
                 <button onClick={() => setOpenModal(false)} className="btn btn-outline-danger">No</button>

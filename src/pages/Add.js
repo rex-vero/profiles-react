15bcc4eb0f024290bee557347a9e80664dbbf64c
unknown <1rexvero1@gmail.com>
2025-03-21 18:40:11 -0700

    react-hook-form added(edit.js remain)

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -4,32 +4,28 @@ import axios from "axios";
 import DataContext from "../contexts/DataContext";
 import { useNavigate } from "react-router-dom";
 import btns from '../assets/scss/Buttons.module.scss';
+import { useForm } from "react-hook-form";
 
 const Add = () => {
     const { setCard } = useContext(DataContext);
-    const [profile, setProfile] = useState({
-        title: '',
-        text: '',
-        img: ''
-    })
+    const { register, formState: { errors, isSubmitting }, handleSubmit, setError, setValue } = useForm();
+    const [photo, setPhoto] = useState(null);
     const navigate = useNavigate();
-    const handleInput = (e) => {
-        const { name, value } = e.target;
-        setProfile(prv => ({ ...prv, [name]: value }))
-    }
     const handleImg = (e) => {
         const file = e.target.files[0];
         const data = new FileReader();
-        data.onload = () => setProfile(prv => ({ ...prv, img: data.result }));
+        data.onloadend = () => {
+            setPhoto(data.result);
+            setValue('base64', data.result);
+        }
         file && file.type.startsWith('image/') ? data.readAsDataURL(file) : console.log('img faild');
     }
-    const handleAdd = async (e) => {
-        e.preventDefault();
+    const handleAdd = async (data) => {
         const newProfile = {
             id: crypto.randomUUID(),
-            title: profile.title,
-            text: profile.text,
-            img: profile.img
+            title: data.title,
+            text: data.text,
+            img: data.base64
         }
         try {
             const { status } = await axios.post('http://localhost:8000/profiles/', JSON.stringify(newProfile), {
@@ -43,7 +39,7 @@ const Add = () => {
                 navigate('/');
             }
         } catch ({ message }) {
-            console.log(message);
+            setError('root', { message });
         }
     }
     useLayoutEffect(() => {
@@ -52,27 +48,27 @@ const Add = () => {
     return (
         <div className={`d-flex justify-content-center align-items-center ${styles.vh90}`}>
             <div className="container-fluid col-11 col-lg-9 card p-3">
-                <form className="d-flex flex-column p-3" onSubmit={handleAdd}>
+                <form className="d-flex flex-column p-3" onSubmit={handleSubmit(handleAdd)}>
                     <div className="mb-3">
-                        <label htmlFor="title" className="form-label">Name</label>
-                        <input type="text" value={profile.title} onChange={handleInput} required autoFocus className="form-control" id="title" name="title" />
+                        <label htmlFor="title" className={`form-label ${errors.title && 'text-danger text-decoration-underline'}`}>{errors.title ? errors.title.message : 'Title'}</label>
+                        <input type="text" {...register('title', { required: 'Title Is Required' })} autoFocus className="form-control" id="title" name="title" />
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="text" className="form-label">Description</label>
-                        <input type="text" value={profile.text} onChange={handleInput} required className="form-control" name="text" id="text" />
+                        <label htmlFor="text" className={`form-label ${errors.text && 'text-danger text-decoration-underline'}`}>{errors.text ? errors.text.message : 'Description'}</label>
+                        <input type="text" {...register('text', { required: 'Description Is Required' })} className="form-control" name="text" id="text" />
                     </div>
                     <div className="mb-3 align-self-center">
-                        <label htmlFor="img" className={`d-flex justify-content-center align-items-center ${btns.btn}`} >
-                            <i className="bi bi-upload" />
+                        <label htmlFor="img" className={`d-flex justify-content-center ${errors.img && `text-danger text-decoration-underline ${styles.pointer}`} align-items-center ${!errors.img && btns.btn}`} >
+                            {errors.img ? errors.img.message : <i className="bi bi-upload" />}
                         </label>
-                        <input type="file" onChange={handleImg} accept="image/*" required className="d-none" name="img" id="img" />
+                        <input type="file" {...register('img', { required: 'Image Is Required', onChange: handleImg })} accept="image/*" className="d-none" name="img" id="img" />
                     </div>
-                    {profile.img && (
+                    {photo && (
                         <div className="mb-3">
-                            <img className={styles.size} src={profile.img} alt={profile.title} />
+                            <img className={styles.size} src={photo} alt={photo} />
                         </div>
                     )}
-                    <button type="submit" className={`bi bi-send-arrow-down fs-5 ${btns.submit}`} />
+                    <button type="submit" disabled={isSubmitting} className={`bi ${(errors.root && 'btn btn-outline-danger') || (isSubmitting ? 'bi-check-circle' : 'bi-send-arrow-down')} fs-5 ${!errors.root && btns.submit}`} >{errors.root && errors.root.message}</button>
                 </form>
             </div>
         </div>
